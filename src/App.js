c97ee2c16f72de6640129108c3af4c60c5587063
suntour <detex.dev@gmail.com>
2020-09-13 02:21:08 -0400

    added material UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import './App.css';
-import TaskList from './components/TaskList';
+import React from "react";
+import "./App.css";
+import TaskList from "./components/TaskList";
+import { Grid, Paper } from "@material-ui/core";
 
 function App() {
   return (
     <div className="App">
-      <TaskList></TaskList>
+      <Grid container justify="center" spacing={3}>
+        <Grid item xs={5}>
+          <Paper className="main-section" elevation={3}>
+            <TaskList></TaskList>
+          </Paper>
+        </Grid>
+      </Grid>
     </div>
   );
 }
--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,30 +1,44 @@
 import React, { useState } from "react";
+import { Button, Grid, Input } from "@material-ui/core";
 
 function TaskForm(props) {
-    const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
 
-    const handleChange = e => {
-        setInput(e.target.value);
-    }
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
 
-    const handleSubmit = e =>{
-        e.preventDefault();
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-        props.onSubmit({
-            id: Math.floor(Math.random() * 1000),
-            text: input
-        });
-        
-        setInput('');
-        
-    };
+    props.onSubmit({
+      id: Math.floor(Math.random() * 1000),
+      text: input,
+    });
+
+    setInput("");
+  };
 
   return (
-      <form className="task-form" onSubmit={handleSubmit}>
-          <input type="text" className='task-input' placeholder= "Add a task" value={input} name="text" onChange={handleChange}></input>
-          <button className='task-button'>Add Task</button>
-      </form>
-  )
+    <form className="task-form" onSubmit={handleSubmit}>
+      <Grid container className="task-form-container" justify="center" alignItems="center" spacing={1}>
+        <Grid item xs={5}>
+          <Input
+            type="text"
+            placeholder="Add a task"
+            name="text"
+            value={input}
+            onChange={handleChange}
+          />
+        </Grid>
+        <Grid item xs={4}>
+          <Button className="add-task-button" type="submit" variant="contained">
+            Add Task
+          </Button>
+        </Grid>
+      </Grid>
+    </form>
+  );
 }
 
 export default TaskForm;
