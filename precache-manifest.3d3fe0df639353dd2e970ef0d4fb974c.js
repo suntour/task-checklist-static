35f07766a2b8cc66a0226cdd5679f49c3a3dc659
suntour <detex.dev@gmail.com>
2020-09-15 18:28:50 -0400

    Updates

---
+++ precache-manifest.3d3fe0df639353dd2e970ef0d4fb974c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71775dbcca2d5f534b749e9628fc0d84",
+    "url": "/task-checklist-static/index.html"
+  },
+  {
+    "revision": "f9fd84914717d1926476",
+    "url": "/task-checklist-static/static/css/main.f9ba01e9.chunk.css"
+  },
+  {
+    "revision": "b612ea926e12cb61e998",
+    "url": "/task-checklist-static/static/js/2.15dfc25f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/task-checklist-static/static/js/2.15dfc25f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9fd84914717d1926476",
+    "url": "/task-checklist-static/static/js/main.60790ce5.chunk.js"
+  },
+  {
+    "revision": "d1933ddc531868964f98",
+    "url": "/task-checklist-static/static/js/runtime-main.1f1d1213.js"
+  },
+  {
+    "revision": "b842e0e120a5f9000ed4ecda3a8a0e4b",
+    "url": "/task-checklist-static/static/media/el_nath_market.b842e0e1.png"
+  }
+]);
